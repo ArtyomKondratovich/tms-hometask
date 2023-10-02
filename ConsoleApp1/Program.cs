9951412e9974ed52f89570edcb87b0a20eb97298
ArtyomKondratovich <kondratovich443@gmail.com>
2023-10-02 12:05:59 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Угадай число");
+
+Console.WriteLine("Я загадал число от 1 до 10, попробуй угадать");
+var hiddenNumber = (new Random()).NextInt64(1, 10);
+
+while (true)
+{
+    try
+    {
+        var usersNumber = long.Parse(Console.ReadLine() ?? string.Empty);
+
+        if (usersNumber == hiddenNumber)
+        {
+            Console.WriteLine("Круто, ты угадал!");
+            break;
+        }
+        else if (usersNumber > 10 || usersNumber < 0)
+        {
+            Console.WriteLine("У тебя промежуток [1, 10]");
+        }
+        else if (usersNumber > hiddenNumber)
+        {
+            Console.WriteLine("Попробуй взять число поменьше");
+        }
+        else
+        {
+            Console.WriteLine("Попробуй взять число побольше");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ты неправильно ввёл число");
+    }
+}
+
+Console.ReadLine();
