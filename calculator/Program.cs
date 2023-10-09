a64f44d5c53b27ab12b6ef68752cb399f12cfab3
ArtyomKondratovich <kondratovich443@gmail.com>
2023-10-09 17:56:22 +0300

    feat: add RPN for calculating expressions

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculator;
+
+Calculator();
+
+void Calculator()
+{
+    ReversePolishNotation reversePolishNotation = new ReversePolishNotation();
+    PrintInstruction();
+
+    while (true)
+    {
+        var inputExpression = GetExpression();
+
+        switch (inputExpression)
+        {
+            case "e":
+                return;
+            case "c":
+                Console.Clear();
+                PrintInstruction();
+                break;
+            default:
+                if (reversePolishNotation.Validate(inputExpression))
+                {
+                    try
+                    {
+                        Console.WriteLine(reversePolishNotation.Calculate());
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                break;
+        }
+    }
+}
+
+string GetExpression()
+{
+    while (true) 
+    {
+        Console.WriteLine("Введите выражение:");
+
+        var inputExpression = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(inputExpression))
+        {
+            Console.WriteLine("Вы ввели пустую строку или только пробелы");
+            continue;
+        }
+
+        return inputExpression;
+    }
+}
+
+void PrintInstruction()
+{
+    Console.WriteLine("e -> exit from calculator\nc -> clear console");
+}
--- calculator/ReversePolishNotation.cs
+++ calculator/ReversePolishNotation.cs
@@ -1,32 +1,146 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
-namespace calculator
+namespace Calculator
 {
     internal class ReversePolishNotation
     {
-        private List<double> _values { get; set; }
+        private Stack<double> _values { get; set; }
 
-        private List<char> _operations { get; set; }
+        private Stack<char> _operations { get; set; }
 
         public ReversePolishNotation()
         {
-            _values = new List<double>();
-            _operations = new List<char>();
+            _values = new Stack<double>();
+            _operations = new Stack<char>();
         }
 
-        public double Calculate(string line)
+        public double Calculate()
+        {
+
+            while (_operations.Count > 0)
+            {
+                var b  = _values.Pop();
+                var a = _values.Pop();
+
+                var operation = _operations.Pop();
+
+                switch (operation)
+                {
+                    case '*':
+                        a *= b;
+                        break;
+                    case '/':
+                        if (b == 0)
+                        {
+                            throw new ArgumentException("Division by zero!");
+                        }
+                        a /= b;
+                        break;
+                    case '^':
+                        a = Math.Pow(a, b);
+                        break;
+                    case '+':
+                        a += b;
+                        break;
+                    case '-': 
+                        a -= b;
+                        break;
+                }
+
+                _values.Push(a);
+            }
+           
+            return _values.Pop();
+        }
+
+        public void Operation(char operation)
+        {
+            var b = _values.Pop();
+            var a = _values.Pop();
+
+            switch (operation)
+            {
+                case '*':
+                    a *= b;
+                    break;
+                case '/':
+                    if (b == 0)
+                    {
+                        throw new ArgumentException("Division by zero!");
+                    }
+                    a /= b;
+                    break;
+                case '^':
+                    a = Math.Pow(a, b);
+                    break;
+                case '+':
+                    a += b;
+                    break;
+                case '-':
+                    a -= b;
+                    break;
+            }
+
+            _values.Push(a);
+            _operations.Pop();
+        }
+
+        public bool Validate(string line)
         {
             _values.Clear();
             _operations.Clear();
 
-            var answer = 0d;
-            //TODO: algorithm convert inputExpression -> reversePolishNotation and calculate
+            // Validate line
+            line = Regex.Replace(line, @"\s", string.Empty);
 
-            return answer;
+            var start = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line.Length - 1 == i && double.TryParse(line[start..], out var parsed))
+                {
+                    _values.Push(parsed);
+                }
+
+                var priority = IsOperator(line[i]);
+                if (priority != 0)
+                {
+                    if (double.TryParse(line[start..i], out var result))
+                    {
+                        _values.Push(result);
+
+                        while (_operations.TryPeek(out var operation) && IsOperator(operation) < priority)
+                        {
+                            Operation(operation);
+                        }
+
+                        _operations.Push(line[i]);
+                        
+                        start = i + 1;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return _values.Count - 1 == _operations.Count;
+        }
+
+        static int IsOperator(char c)           /* if c is operator return his priority else return 0 */ 
+        {
+            switch (c) 
+            {
+                case '+':
+                case '-':
+                    return 2;
+                case '^':
+                case '/':
+                case '*':
+                    return 1;
+                default:
+                    return 0;
+            }
         }
     }
 }
